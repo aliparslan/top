2b851f76b0677f8eb438ae3e49237acef5cb66c5
Alip Arslan <40613474+aliparslan@users.noreply.github.com>
2023-07-28 08:29:20 -0400

    update rps-revisited script.js

--- rps-revisited/script.js
+++ rps-revisited/script.js
@@ -1,4 +1,19 @@
 choices = ['Rock', 'Paper', 'Scissors'];
+player_wins = 0;
+rudy_wins = 0;
+
+results = document.querySelector('#results');
+game_over = document.querySelector('.game-over');
+remove_list = document.querySelectorAll('.remove');
+
+player_text = document.querySelector('#player-wins');
+rudy_text = document.querySelector('#rudy-wins');
+angry_rudy = document.createElement('img');
+angry_rudy.src = 'rudy_angry.jpg';
+angry_rudy.classList.add('rudy');
+happy_rudy = document.createElement('img');
+happy_rudy.src = 'rudy_happy.jpg';
+happy_rudy.classList.add('rudy');
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random()*choices.length)];
@@ -7,37 +22,80 @@ function getComputerChoice() {
 function playRound (player, computer) {
     player = player.toLowerCase();
     computer = computer.toLowerCase();
+
+    result = '';
+    const div = document.createElement('div');
+
+
     if ((player === 'rock' && computer === 'paper')
         || (player === 'scissors' && computer === 'rock')
         || (player === 'paper' && computer === 'scissors')
     ) {
-        return `You lose! ${computer} beats ${player}`;
+        result = `You lose! ${computer} beats ${player}`;
+        rudy_wins += 1;
+        rudy_text.textContent = `Rudy's wins: ${rudy_wins}`;
     }
     else if (player === computer) {
-        return "It's a tie!";
+        result = "It's a tie!";
     }
     else {
-        return `You win! ${player} beats ${computer}`;
+        result = `You win! ${player} beats ${computer}`;
+        player_wins += 1;
+        player_text.textContent = `Your wins: ${player_wins}`;
     }
-}
+    div.textContent = result;
+    results.insertBefore(div, results.firstChild);
 
-function game() {
-    // player_wins = 0;
-    // computer_wins = 0;
-    for (i = 0; i < 5; i++) {
-        player = prompt('What is your choice?');
-        console.log(playRound(player, getComputerChoice()));
+    if ((rudy_wins === 10)) {
+        remove_list.forEach(item => item.remove());
+
+        text = document.createElement('p');
+        text.textContent = "YOU'RE FIRED!"
+        text.style.color = 'red';
+        text.style.fontSize = '50px';
+        text.style.fontWeight = 'bold';
+        
+        game_over.appendChild(angry_rudy);
+        game_over.appendChild(text);
     }
+
+    if ((player_wins === 10)) {
+        remove_list.forEach(item => item.remove());
+
+        text = document.createElement('p');
+        text.textContent = "YOU'RE PROMOTED!"
+        text.style.color = 'green';
+        text.style.fontSize = '50px';
+        text.style.fontWeight = 'bold';
+        
+        game_over.appendChild(happy_rudy);
+        game_over.appendChild(text);
+    }
+
+    // if (rudy_wins === 10) {
+    //     wins.textContent = "YOU'RE FIRED!"
+    //     wins.style.color = 'red';
+    //     wins.style.fontSize = '50px';
+    // }
+
+    // if (player_wins === 10) {
+    //     wins.textContent = "YOU'RE PROMOTED!"
+    //     wins.style.color = 'green';
+    //     wins.style.fontSize = '50px';
+    // }
 }
-
-// console.log(game());
-
-results = document.querySelector('.results');
-
-const div = document.createElement('div');
-div.textContent = 'hello!';
 
 rock = document.querySelector('#rock');
-rock.onclick = results.appendChild(div);
+rock.addEventListener('click',  function() {
+    playRound('rock', getComputerChoice());
+});
 
-results.appendChild(div);
+rock = document.querySelector('#paper');
+rock.addEventListener('click',  function() {
+    playRound('paper', getComputerChoice());
+});
+
+rock = document.querySelector('#scissors');
+rock.addEventListener('click',  function() {
+    playRound('scissors', getComputerChoice());
+});
